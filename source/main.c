7fb093cf34c9f4a6b47f9e231651c9ff99b59cea
Kolitero <isayannogueira@gmail.com>
2022-10-26 17:33:30 -0300

    caralho

--- source/main.c
+++ source/main.c
@@ -29,7 +29,7 @@ void init(GtkApplication *app,gpointer user_data){
 int main(int argc,char **argv) {
 	GtkApplication *app;
 	int status;
-	app = gtk_application_new("br.isayan",G_APPLICATION_DEFAULT_FLAGS);
+	app = gtk_application_new("br.isayan",G_APPLICATION_FLAGS_NONE);
 	g_signal_connect(app,"activate",G_CALLBACK(init),NULL);
 	status = g_application_run (G_APPLICATION (app), argc, argv);
   	g_object_unref (app);
